304141805fac8f5bbf19987390fcf2dba56017d2
Francisco Lizcano <flizcanor@icloud.com>
2025-06-07 08:41:51 -0500

    Intro ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,11 +22,7 @@
             .content-wrapper.px-lg-4.pt-lg-4.px-xxl-5.py-xxl-5.text-start
               p.home-main-title UNIDAD {{ `${globalData.numeroUnidad}` }}
               h1.home-title.mb-4 {{ globalData.tituloUnidad }}
-              p.descripcion.mb-4 Desde los comienzos de la humanidad, la relación entre los principios éticos, el desarrollo económico y la sostenibilidad ha sido fundamental para garantizar un progreso equilibrado y equitativo. En un contexto globalizado, donde los desafíos éticos y ambientales son cada vez más evidentes, la bioética emerge como una herramienta clave para orientar decisiones económicas responsables que respeten tanto el medio ambiente como los derechos humanos.
-
-              p.descripcion.mb-4 En esta unidad, profundizaremos en el rol de la bioética como pilar de la sostenibilidad en el desarrollo económico, explorando temas esenciales como la aplicación de principios éticos al uso de recursos naturales, la influencia de las políticas globales en la protección de los derechos humanos y la ética en la innovación tecnológica. 
-              
-              p #[b Prepárate para explorar los retos y oportunidades que la sostenibilidad y la bioética presentan en un mundo en constante evolución.]
+              p.descripcion.mb-4 La Unidad 3 del curso #[strong Fundamentos de Programación] se enfoca en el uso de funciones y la modularidad como herramientas clave para estructurar, organizar y escalar software de forma eficiente. Se abordan tres temas centrales: declaración y uso de funciones, recursión y modularidad en proyectos. Cada sección desarrolla habilidades esenciales como reutilización de código, organización mediante módulos, y comprensión de conceptos como alcance de variables, tipos de recursión y gestión de dependencias. El estudiante aprenderá a diseñar programas claros, mantenibles y reutilizables con buenas prácticas de programación.
 
               .button-container.mt-auto
                 router-link.btn-iniciar.w-100.w-md-auto(:to="{ name: 'introduccion' }")
@@ -154,7 +150,7 @@ export default {
   .icon
     transition: all 0.25s ease-in-out
     // background-color: #083E61 !important
-    background-color: darken($color-acento-botones, 30% ) !important
+    background-color: #5C2C88 !important
   &:hover
     .icon
       transform: scale(1.06)
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -1,6 +1,34 @@
 <template lang="pug">
 .curso-main-container.introduccion
   BannerInterno(subTitulo="Introducción")
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-
+    .row.mb-5
+      .col-lg-8
+        p(data-aos="fade-left").mb-4 En el desarrollo de software, la capacidad de estructurar el código de manera reutilizable es fundamental para construir programas escalables y mantenibles. En este contexto, las funciones y la modularidad cumplen un papel clave, ya que permiten dividir un problema en partes más pequeñas y manejables, facilitando tanto la organización del código como su comprensión y mantenimiento.  
+    
+        .bg-color-1.p-4(data-aos="fade-left").mb-4
+          .row.align-items-start
+            .col-lg-auto
+              img(src="@/assets/curso/intro/1.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 Esta unidad proporciona una base sólida en el uso de funciones y el diseño modular, abordando tres aspectos esenciales. Primero, se estudiará la declaración y uso de funciones, explorando cómo encapsular bloques de código que pueden ejecutarse múltiples veces sin necesidad de repetición, lo que mejora la claridad y organización de los programas.
+    
+
+    
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/intro/2.png", data-aos="zoom-in")
+
+    .row.mb-5.align-items-center
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/intro/3.png", data-aos="zoom-in")
+      .col-lg-8
+
+        p(data-aos="fade-left").mb-4 Luego, se analizará el concepto de recursión, una técnica en la que una función se llama a sí misma para resolver problemas de manera estructurada. Se examinarán los principios detrás de este enfoque, sus aplicaciones más comunes y las mejores prácticas para evitar errores.
+    
+        .bg-color-8.p-4(data-aos="fade-left")
+          p.mb-0 Finalmente, se profundizará en la modularidad en proyectos, un principio clave en el diseño de software que permite dividir grandes sistemas en componentes más pequeños e independientes. Se estudiará cómo organizar el código en módulos, gestionar dependencias y facilitar la colaboración en entornos de desarrollo.
+    
 
 </template>
